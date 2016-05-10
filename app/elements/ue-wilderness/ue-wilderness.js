d946f7394408f205e7272f1e3bbb6ccf7540c8da
Bratislav Petrovic <bratislav.petrovic@gmail.com>
2016-05-10 18:31:23 +0200

    wilderness

---
+++ app/elements/ue-wilderness/ue-wilderness.js
@@ -0,0 +1,16 @@
+/*global Polymer */
+
+(function() {
+    'use strict';
+
+    Polymer({
+      is: 'ue-wilderness',
+
+      properties: {
+        game: {
+          type: Object,
+          notify: true
+        }
+      }
+    });
+}());
